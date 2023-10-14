14779ba8e2cc038c1f7c92e695198f98e484d001
Berland1 <petrenko.arseniy@bk.ru>
2023-10-14 18:49:45 +0300

    added console.log

---
+++ PR5/8.5.py
@@ -0,0 +1,3 @@
+string = input('Введите строку с точкой на конце :')
+words = string.split()
+print('Количество слов в строке: ', len(words))
---
+++ PR5/9,5.py
@@ -0,0 +1,4 @@
+string = input('Введите строку :')
+words_split = string.split()
+word_count = words_split.count('Пока')
+print('Количество слов "Привет" в строке : ', word_count)
